15bff9a8e09657dfb83acadf227a5935251bd20c
olimov Amir <olimov.amir@mail.ru>
2023-11-24 10:11:45 +0300

    Установлен параметр бота

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
 ﻿
+using PRTelegramBot.Core;
+
+const string EXIT_COMMAND = "exit";
+
+var telegram = new PRBot(option =>
+{
+    option.Token = "6596108797:AAEYt947sJtftpQeiFI-QuVEM8UEatzB6vM";
+    option.ClearUpdatesOnStart = true;
+    option.WhiteListUsers = new List<long>() {};
+    option.Admins = new List<long>() {};
+    option.BotId = 0;
+});
+
+while (true)
+{
+    var result = Console.ReadLine();
+    if (result.ToLower() == EXIT_COMMAND)
+    {
+        Environment.Exit(0);
+    }
+}
